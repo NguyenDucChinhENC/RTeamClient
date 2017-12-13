1fea653c937a124c24a062995e3c77215a37cf91
Admin <admin@Ebolaa.local>
2017-12-13 17:50:02 +0700

    update ui simple

--- src/app/group/group-thumbnail/group-thumbnail.component.ts
+++ src/app/group/group-thumbnail/group-thumbnail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GroupService } from '../group.service'
 import { ActivatedRoute , Router } from '@angular/router';
+import { IMG_URL } from '../../constants';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 declare const $: any;
 declare interface ButtonMember {
@@ -27,16 +29,20 @@ export class GroupThumbnailComponent implements OnInit {
   current_user: any = {};
   info_group: any = {};
   member_button: any = {};
+  member_total: any =0;
+  event_total: any=0;
   status_user: number = 3;
   is_admin: boolean = false;
+  id_member_group = -1;
+  cover: any;
   events: any = [];
 
   constructor(
     private groupSevice: GroupService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private _sanitizer: DomSanitizer
     ) { }
-
   ngOnInit() {
     this.getGroupThumbnail();
     
@@ -52,8 +58,14 @@ export class GroupThumbnailComponent implements OnInit {
 
   onSuccessGetGroupInfo(response){
     this.info_group = response.data.group;
+    if (this.info_group.cover.url) {
+      this.cover= this.info_group.cover.url +"";
+    }
     this.is_admin = response.data.admin;
     this.events = response.data.events;
+    this.member_total = response.data.member_total;
+    this.event_total = response.data.event_total;
+    this.id_member_group = response.data.id_membered_group;
     if (response.data.membered == true){
       this.member_button = MemberButton.filter(memberButton => memberButton)[0];
       this.status_user =0;
@@ -74,13 +86,52 @@ export class GroupThumbnailComponent implements OnInit {
   }
 
   action() {
-    if (this.status_user == 2)
+    if (this.status_user == 2){
     this.groupSevice.joinGroup(this.current_user.authentication_token,this.id_group).
       subscribe(response => this.onJoinGroupSuccess(response))
+    } else {
+      if (this.status_user == 1 || this.status_user == 0){
+        this.groupSevice.cancelJoinGroup(this.current_user.authentication_token,this.id_member_group).
+        subscribe(response => this.onJoinGroupSuccess(response))
+      }
+    }
   }
 
   onJoinGroupSuccess(response){
     this.getGroupThumbnail();
     console.log("want join");
   }
+
+  onChange(e) {
+    if (e.target.files && e.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = this.changeImage.bind(this);
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  }
+
+  changeImage(e) {
+    const image = <FileReader> e.target;
+    const ava = {
+      group: {
+        cover: image.result
+      }
+    };
+    const token = JSON.parse(localStorage.getItem('currentUser')).authentication_token;
+    this.groupSevice.changeCover(ava, this.id_group, token).
+      subscribe(response => this.onChangeSuccess(response, image),
+      response => this.onChangeError());
+  }
+
+  onChangeSuccess(response, image) {
+    this.router.navigate(['/groups',this.id_group])
+  }
+
+  onChangeError() {
+    $("#coverGroupChange").modal("hide");
+  }
+
+  getBackground(image) {
+    return this._sanitizer.bypassSecurityTrustStyle(`url(${image})`);
+}
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProfileService } from './profile.service';
 import { User } from './user';
+import { IMG_URL } from '../constants';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css'],
+  styleUrls: ['./user.component.scss'],
   providers: [ProfileService],
 })
 export class UserComponent implements OnInit {
@@ -28,6 +29,9 @@ export class UserComponent implements OnInit {
     this.profileService.getInfoUser(this.user_id).subscribe(
       (user) => {
         this.currentUser = user;
+        if (this.currentUser.avatar) {
+          this.currentUser.avatar = IMG_URL + this.currentUser.avatar;
+        }
         console.log(this.currentUser.typeof)
       },
       response => this.onError,
@@ -43,6 +47,56 @@ export class UserComponent implements OnInit {
   
   onError() {
     this.router.navigate(['']);
+  }
+
+  onClickUpdate(value: any){
+    console.log(value);
+    const token = JSON.parse(localStorage.getItem('currentUser')).authentication_token;
+    this.profileService.updateUser(value, this.user_id, token).subscribe(
+      response => this.onNext_update(response),
+      response => this.onError_update(response)
+    )
+  }
+
+  onNext_update(response){
+    console.log(response)
+  }
+
+  onError_update(response){
+    console.log(response)
+  }
+
+  chooseImage(id: string) {
+    $(id).trigger('click');
+  }
+
+  onChange(e) {
+    if (e.target.files && e.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = this.changeImage.bind(this);
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  }
+
+  changeImage(e) {
+    const image = <FileReader> e.target;
+    const ava = {
+      user: {
+        avatar: image.result
+      }
+    };
+    const token = JSON.parse(localStorage.getItem('currentUser')).authentication_token;
+    this.profileService.changeAvatar(ava, this.user_id, token).
+      subscribe(response => this.onChangeSuccess(response, image),
+      response => this.onChangeError());
+  }
+
+  onChangeSuccess(response, image) {
+
+  }
+
+  onChangeError() {
+  
   }
 
 }
